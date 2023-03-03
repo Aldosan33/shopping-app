215e82049bc5f25bdef825f108a402aaa06e1508
alfredo sanchez alvarez <alfredo_2993@hotmail.com>
2023-03-03 00:51:01 -0500

    Add shopping-app source code

---
+++ shopping-app/ShoppingApp.Client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Client.Data;
+using ShoppingApp.Client.Models;
+using System.Diagnostics;
+
+namespace ShoppingApp.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductData.products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ shopping-app/ShoppingApp.Client/Data/ProductData.cs
@@ -0,0 +1,43 @@
+﻿using ShoppingApp.Client.Models;
+
+namespace ShoppingApp.Client.Data
+{
+    public class ProductData
+    {
+        public static readonly IEnumerable<Product> products = new List<Product>
+        {
+          new Product
+          {
+            Name = "IPhone X",
+            Description = "This phone is the tenth version of Apple mobiles",
+            ImageFile = "product-1.png",
+            Price = 950.00M,
+            Category = "IPhone"
+          },
+          new Product
+          {
+            Name = "Samsung M20",
+            Description = "This phone is the newest generation of Samsung mobiles",
+            ImageFile = "product-2.png",
+            Price = 1030.00M,
+            Category = "Samsung"
+          },
+          new Product
+          {
+            Name = "Huawei Mate 20",
+            Description = "This phone is the most promising device of Huawei company",
+            ImageFile = "product-3.png",
+            Price = 600.00M,
+            Category = "Huawei"
+          },
+          new Product
+          {
+            Name = "Xiaomi Mi 9",
+            Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+            ImageFile = "product-4.png",
+            Price = 380.00M,
+            Category = "White Appliances"
+          }
+        };
+    }
+}
